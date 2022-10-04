8d23bcd1eaa8b18dc64322db2402654afb71d830
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-10-04 10:24:33 +0200

    AddBookServlet and Book class

--- src/main/java/no/edu/library/AddBookServlet.java
+++ src/main/java/no/edu/library/AddBookServlet.java
@@ -4,13 +4,21 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.eclipse.jetty.servlet.Source;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class AddBookServlet extends HttpServlet {
+    private static final Logger logger = LoggerFactory.getLogger(AddBookServlet.class);
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var bookTitle = req.getParameter("bookTitle");
+        var bookAuthor = req.getParameter("bookAuthor");
+        var book = new Book(bookAuthor, bookTitle);
 
+        logger.info("Adding book -> {}", book);
     }
+
 }
