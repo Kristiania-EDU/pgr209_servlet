43111bde23b40e9acd49835b904f82d00433dd31
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-10-04 09:51:11 +0200

    Added "AddBookServlet" to WebApp

---
+++ src/main/java/no/edu/library/AddBookServlet.java
@@ -0,0 +1,16 @@
+package no.edu.library;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.servlet.Source;
+
+import java.io.IOException;
+
+public class AddBookServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
--- src/main/java/no/edu/library/LibraryServer.java
+++ src/main/java/no/edu/library/LibraryServer.java
@@ -1,6 +1,7 @@
 package no.edu.library;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
@@ -11,12 +12,12 @@ public class LibraryServer {
     private final Server server = new Server(9080);
 
     public void start() throws Exception {
-        server.setHandler(new WebAppContext(
-            Resource.newClassPathResource("/webapp"), "/"));
+        var webApp = new WebAppContext(
+            Resource.newClassPathResource("/webapp.library"), "/");
 
-        server.setHandler(new WebAppContext(
-            Resource.newClassPathResource("/webapp.library"), "/library"));
+        webApp.addServlet(new ServletHolder(new AddBookServlet()), "/api/addBook");
 
+        server.setHandler(webApp);
         server.start();
         logger.info("Started server on {}", server.getURI());
     }
